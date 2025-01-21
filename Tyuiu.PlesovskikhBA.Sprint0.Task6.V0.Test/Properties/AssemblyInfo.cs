5b17eb2a648555f30ae0396bc2c43cb87e0e0790
Tyuiu.PlesovskikhBA@outlook.com <Tyuiu.PlesovskikhBA@outlook.com>
2025-01-21 20:45:29 +0500

    Task6

---
+++ Tyuiu.PlesovskikhBA.Sprint0.Task6.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PlesovskikhBA.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PlesovskikhBA.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("149a5db9-caad-4f24-ba08-f0adc69c1c6c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
